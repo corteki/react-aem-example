a85fc2d607bb3ba5678ba71d9e792e7e916f73ab
tim alaerts <timalaerts1990@gmail.com>
2020-01-08 13:16:00 +0100

    extended the ResponsiveGrid class and serialized the Model

--- core/src/main/java/com/tim/core/models/ContainerModel.java
+++ core/src/main/java/com/tim/core/models/ContainerModel.java
@@ -1,8 +1,12 @@
 package com.tim.core.models;
 
+import java.util.Map;
+
 import com.adobe.cq.export.json.ComponentExporter;
+import com.adobe.cq.export.json.ContainerExporter;
 import com.adobe.cq.export.json.ExporterConstants;
 import com.day.cq.wcm.foundation.model.responsivegrid.ResponsiveGrid;
+import com.day.cq.wcm.foundation.model.responsivegrid.export.ResponsiveGridExporter;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import org.apache.sling.api.SlingHttpServletRequest;
@@ -12,37 +16,37 @@ import org.apache.sling.models.annotations.Optional;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 @Model(
-    adaptables = SlingHttpServletRequest.class,
-    adapters = {ContainerModel.class, ResponsiveGrid.class, ComponentExporter.class},
-    resourceType = ContainerModel.RESOURCE_TYPE
+    adaptables = SlingHttpServletRequest.class, 
+    adapters = { ComponentExporter.class }, 
+    resourceType = { ContainerModel.RESOURCE_TYPE }
 )
 @Exporter(
-    name = ExporterConstants.SLING_MODEL_EXPORTER_NAME,
+    name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, 
     extensions = ExporterConstants.SLING_MODEL_EXTENSION
 )
 @JsonSerialize(as = ContainerModel.class)
-public class ContainerModel implements ComponentExporter {
-
+public class ContainerModel extends ResponsiveGrid implements ComponentExporter {
     static final String RESOURCE_TYPE = "react-typescript-demo/components/container";
-
     private final String DEFAULT_AMOUNT = "1";
 
-    @ValueMapValue @Optional
-    private String rows;
+    @ValueMapValue
+    @Optional
+    private String rowAmount;
 
-    @ValueMapValue @Optional
-    private String columns;
+    @ValueMapValue
+    @Optional
+    private String columnAmount;
 
-    public String getColumns() {
-        if(columns != null && columns.length() > 0) {
-            return columns;
+    public String getColumnAmount() {
+        if (columnAmount != null && columnAmount.length() > 0) {
+            return columnAmount;
         }
         return DEFAULT_AMOUNT;
     }
 
-    public String getRows() {
-        if(rows != null && rows.length() > 0) {
-            return rows;
+    public String getRowAmount() {
+        if (rowAmount != null && rowAmount.length() > 0) {
+            return rowAmount;
         }
         return DEFAULT_AMOUNT;
     }
