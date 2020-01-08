fd6181b39d793e1b253429aa921604ac3faa1a34
tim alaerts <timalaerts1990@gmail.com>
2020-01-08 15:14:54 +0100

    removed context

--- client/src/context/AppContext.ts
+++
@@ -1,8 +0,0 @@
-import React, {createContext} from 'react';
-
-export const AppContext = createContext({
-    products: [],
-    cart: [],
-    addToCart: (product: any) => {},
-    removeFromCart: (id: number) => {}
-} as any);
